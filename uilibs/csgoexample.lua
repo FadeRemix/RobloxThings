8ebad9ba2bb308b4d41ceb9fd27a3164e292766a
fade.rip <64497456+FadeRemix@users.noreply.github.com>
2025-05-28 19:25:43 -0400

    Update csgoexample.lua

--- uilibs/csgoexample.lua
+++ uilibs/csgoexample.lua
@@ -1,5 +1,5 @@
 --aaa
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/i77lhm/vaderpaste/refs/heads/main/library.lua"))() 
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/FadeRemix/RobloxThings/refs/heads/main/libReqs/csgo.lua"))() 
 local flags = library.flags -- access flags from here.
 
 local window = library:window({
